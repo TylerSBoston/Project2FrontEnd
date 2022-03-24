0865e0c044c51a3250eecc55539f63f6127f7a98
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-24 15:39:09 -0400

    aa

---
+++ src/app/reimbursement/image.model.ts
@@ -0,0 +1,10 @@
+export interface Image{
+    imageId: number,
+    reimbursementId: number,
+    contentType: String,
+    size: number,
+    name: string,
+    
+   // date of most recent update, including if resolved
+
+}
